55da5391b014f2b739036fad54a5971f583ebcb5
DanKStern <dankstern@gmail.com>
2026-03-19 09:19:17 -0500

    Added Button and Welcome Functions

--- react-essentials/hello-world/src/App.jsx
+++ react-essentials/hello-world/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
 
+function Welcome() {
+  return <h2>Welcome Dan</h2>;
+}
+
+function Button() {
+  return <button>Click Me</button>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Welcome />
     </div>
   );
 }
