f63237d3ea76a50e7d29f00ef9826064f931d75f
DanKStern <dankstern@gmail.com>
2026-03-21 15:48:49 -0500

    fixing HelloWithoutJSX

--- react-essentials/hello-world/src/Hello.jsx
+++ react-essentials/hello-world/src/Hello.jsx
@@ -1,7 +1,17 @@
+import React from "react";
+
 export const Hello = () => {
   return (
     <div id="container">
       <h1>Hello Dan</h1>
     </div>
+  );
+};
+
+export const HelloWithoutJSX = () => {
+  return React.createElement(
+    "div",
+    { id: "container" },
+    React.createElement("h1", null, "Hello Dan"),
   );
 };
