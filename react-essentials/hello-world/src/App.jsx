39dca561e604cb0415bc0e07521956782a6a9e1d
DanKStern <dankstern@gmail.com>
2026-03-22 09:49:37 -0500

    Sunday 3/22

--- react-essentials/hello-world/src/App.jsx
+++ react-essentials/hello-world/src/App.jsx
@@ -3,10 +3,18 @@ import "./App.css";
 import { Welcome } from "./Welcome";
 import { Button } from "./Button";
 import { Hello, HelloWithoutJSX } from "./Hello";
+import { UserProfile } from "./UserProfile";
+import { ContactForm } from "./ContactForm";
+import { StyledForm } from "./StyledForm";
+import "./App.css";
 
+<App className="css"></App>;
 function App() {
   return (
     <div className="App">
+      <StyledForm />
+      <UserProfile />
+      <ContactForm />
       <Hello />
       <HelloWithoutJSX />
       <Welcome />
