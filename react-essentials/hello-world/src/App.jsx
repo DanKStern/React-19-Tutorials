d37919a92890a5616d5aaa862c510f1784565105
DanKStern <dankstern@gmail.com>
2026-03-25 09:14:41 -0500

    Wed AM

--- react-essentials/hello-world/src/App.jsx
+++ react-essentials/hello-world/src/App.jsx
@@ -7,12 +7,18 @@ import { StyledForm } from "./StyledForm";
 import { CandidateProfile } from "./CandidateProfile";
 import { Product } from "./Product";
 import { Greeting } from "./Greeting";
+import { CardWrapper } from "./CardWrapper";
 import "./App.css";
 
 <App className="css"></App>;
 function App() {
   return (
     <div className="App">
+      <CardWrapper title="User Information">
+        <p>Bruce Wayne</p>
+        <p>batman@gothamcity.com</p>
+        <Button>Edit Profile</Button>
+      </CardWrapper>
       <Greeting name="Bruce" Greeting="Good Morning" />
       <Greeting message="Welcome!" />
       <Greeting name="Diana" />
